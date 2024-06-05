65793ecc4007b0b188c7ceac091cc5a480351df6
ErickV125 <erickvegardz22@gmail>
2024-06-05 00:44:39 -0600

    Modificacion

--- ScriptsJs/Products/DataBase-Products.js
+++ ScriptsJs/Products/DataBase-Products.js
@@ -1,5 +1,5 @@
 let product = [ ]
-for (let i = 0; i < 8; i++) {
+for (let i = 0; i < 100; i++) {
     let product1 = new CreateProduct("Name", 10000, ["Iconos/Default.webp"], 12, "Product Default", "Seller", ["Tags"], 5000);
     product.push(product1);
 }
